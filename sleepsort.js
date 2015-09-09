13e777d3a1b6c3edce49d559cbefc72bf30d6b14
qingwei.li <qingwei.li@ifanr.com>
2015-09-09 23:00:04 +0800

    add sleepsort

---
+++ sleepsort.js
@@ -0,0 +1,22 @@
+/**
+ * @description 
+ * 睡觉排序
+ * http://rosettacode.org/wiki/Sorting_algorithms/Sleep_sort
+ * 
+ * @return {array} 由小到大排序的数组
+ * 
+ * @example
+ * 
+ * var arr = [6,4,2,8,1,2].sleepsort();
+ * console.log(arr); // [1,2,2,4,6,8]
+ * 
+ */
+Array.prototype.sleepsort = function () {
+  var _arr = [];
+  this.forEach(function (number) {
+    setTimeout(function () {
+      _arr.push(number);
+    }, 2 * number);
+  });
+  return _arr;
+}
